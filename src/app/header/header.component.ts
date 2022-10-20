a66e1fc24fb049b75adc262a770875727bb16d5b
Baychura Vitaliy <vitaliy.baychura@gmail.com>
2022-10-20 12:39:48 +0300

    fix: remove unused code

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
-import { ApiMoviesService } from '../api-movies.service';
 
 @Component({
   selector: 'app-header',
